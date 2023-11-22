14a75a9b4ae7362dd19d936df1aae787da1ab2f8
Guilherme-silva0 <guilhermesilvadev0@gmail.com>
2023-11-22 11:32:22 -0300

    Categorias dinâmicas com viewShare

--- resources/views/site/layout.blade.php
+++ resources/views/site/layout.blade.php
@@ -12,12 +12,22 @@
 
 <body>
 
+    {{-- Dropdown structure --}}
+
+    <ul id="dropdown1" class="dropdown-content">
+        @foreach ($categories as $category)
+            <li><a href="#!">{{ $category->name }}</a></li>
+        @endforeach
+    </ul>
+
     <nav class="red">
         <div class="nav-wrapper container">
             <a href="#" class="brand-logo center">Show</a>
             <ul id="nav-mobile" class="left">
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Carrinho</a></li>
+                <li><a href={{ route('site.index') }}>Home</a></li>
+                <li><a href="">Carrinho</a></li>
+                <li><a href="" class="dropdown-trigger" data-target="dropdown1"> Categorias <i
+                            class="material-icons right">expand_more</i></i> </a></li>
             </ul>
         </div>
     </nav>
@@ -25,6 +35,15 @@
     @yield('content')
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
+    <script>
+        /* Dropdown */
+        var elemDrop = document.querySelectorAll('.dropdown-trigger');
+        var instanceDrop = M.Dropdown.init(elemDrop, {
+            coverTrigger: false,
+            constrainWidth: false
+        });
+    </script>
 </body>
 
 </html>
