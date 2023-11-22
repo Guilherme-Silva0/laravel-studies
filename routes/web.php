b29abbdd1c724ae5d3eae70d9744b276b354ebcc
Guilherme-silva0 <guilhermesilvadev0@gmail.com>
2023-11-22 16:08:04 -0300

    Carrinho de compras com darryldecode/cart

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CartController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\SiteController;
@@ -81,3 +82,4 @@ Route::resource('products', ProductController::class);
 Route::get('/', [SiteController::class, 'index'])->name('site.index');
 Route::get('/product/{slug}', [SiteController::class, 'details'])->name('site.details');
 Route::get('/category/{id}', [SiteController::class, 'category'])->name('site.category');
+Route::get('/cart', [CartController::class, 'cartList'])->name('site.cart');
