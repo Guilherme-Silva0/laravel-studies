96a07678b41da883baa2523327b4080571669329
Guilherme-silva0 <guilhermesilvadev0@gmail.com>
2023-11-28 12:27:27 -0300

    feat: :sparkles: list all supports

--- second-course/resources/views/admin/supports/index.blade.php
+++ second-course/resources/views/admin/supports/index.blade.php
@@ -1,3 +1,25 @@
 <h1>Listagem dos suportes</h1>
 
 <a href="{{ route('supports.create') }}">Criar suporte</a>
+
+
+<table>
+    <thead>
+        <th>Assunto</th>
+        <th>Status</th>
+        <th>Descrição</th>
+        <th></th>
+    </thead>
+    <tbody>
+        @foreach ($supports as $support)
+            <tr>
+                <td>{{ $support->subject }}</td>
+                <td>{{ $support->status }}</td>
+                <td>{{ $support->body }}</td>
+                <td>
+                    >
+                </td>
+            </tr>
+        @endforeach
+    </tbody>
+</table>
