6812083a149765fcb76a45673881b7b26eaa75f9
Guilherme-silva0 <guilhermesilvadev0@gmail.com>
2023-11-29 10:40:20 -0300

    feat: :sparkles: validate store support

--- second-course/resources/views/admin/supports/create.blade.php
+++ second-course/resources/views/admin/supports/create.blade.php
@@ -1,8 +1,16 @@
 <h1>Nova dúvida</h1>
 
+@if ($errors->any())
+    <ul>
+        @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+@endif
+
 <form action="{{ route('supports.store') }}" method="post">
     @csrf
-    <input type="text" name="subject" placeholder="Assunto">
-    <textarea name="body" placeholder="Descrição" cols="30" rows="5"></textarea>
+    <input type="text" name="subject" placeholder="Assunto" value="{{ old('subject') }}">
+    <textarea name="body" placeholder="Descrição" cols="30" rows="5">{{ old('body') }}</textarea>
     <button type="submit">Enviar</button>
 </form>
