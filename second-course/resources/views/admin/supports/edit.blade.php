a4f6404a4c18a15547060c9a438e37b3bdd6dc67
Guilherme-silva0 <guilhermesilvadev0@gmail.com>
2023-11-30 10:04:16 -0300

    refactor: :recycle: use components and reuse forms

--- second-course/resources/views/admin/supports/edit.blade.php
+++ second-course/resources/views/admin/supports/edit.blade.php
@@ -1,17 +1,11 @@
 <h1>Editar dúvida - {{ $support->id }}</h1>
 
-@if ($errors->any())
-    <ul>
-        @foreach ($errors->all() as $error)
-            <li>{{ $error }}</li>
-        @endforeach
-    </ul>
-@endif
+<x-alert />
 
 <form action="{{ route('supports.update', $support->id) }}" method="post">
     @method('put')
-    @csrf
-    <input type="text" name="subject" placeholder="Assunto" value="{{ $support->subject }}">
-    <textarea name="body" placeholder="Descrição" cols="30" rows="5">{{ $support->body }}</textarea>
-    <button type="submit">Enviar</button>
+    @include('admin.supports.partials.form', [
+        'buttonText' => 'Editar',
+        'support' => $support,
+    ])
 </form>
