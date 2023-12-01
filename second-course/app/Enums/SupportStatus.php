1e1b943040eb1b1276996b90018bdc4be7bf84a3
Guilherme-silva0 <guilhermesilvadev0@gmail.com>
2023-12-01 11:41:57 -0300

    feat: :sparkles: create enum status supports

---
+++ second-course/app/Enums/SupportStatus.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Enums;
+
+enum SupportStatus: string
+{
+    case A = "Open";
+    case P = "Pendent";
+    case C = "Closed";
+
+    public static function fromValue(string $value): string
+    {
+        foreach (self::cases() as $status) {
+            if($value === $status->name) {
+                return $status->value;
+            }
+        }
+
+        throw new \ValueError("$value is not a valid");
+        
+    }
+}
