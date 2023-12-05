5adfbe9eb2582bf7abedd1a5c2f0a0225a149605
Guilherme-silva0 <guilhermesilvadev0@gmail.com>
2023-12-05 16:04:21 -0300

    feat: :sparkles: auth api with sanctum

--- second-course/routes/api.php
+++ second-course/routes/api.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\Api\Auth\AuthController;
 use App\Http\Controllers\Api\SupportController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('/supports', SupportController::class);
+Route::post('/auth', [AuthController::class, 'auth']);
+
+Route::middleware(["auth:sanctum"])->group(function () {
+    Route::apiResource('/supports', SupportController::class);
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/me', [AuthController::class, 'me']);
+});
