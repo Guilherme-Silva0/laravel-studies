eb12924c6393bec74952d2a72e7461ea885c9d3a
Guilherme-silva0 <guilhermesilvadev0@gmail.com>
2023-12-06 19:04:36 -0300

    refactor: :recycle: move file

--- second-course/app/DTO/Supports/CreateSupportDTO.php
+++ second-course/app/DTO/Supports/CreateSupportDTO.php
@@ -1,24 +1,22 @@
 <?php
 
-namespace App\DTO;
+namespace App\DTO\Supports;
 
 use App\Enums\SupportStatus;
 use App\Http\Requests\StoreUpdateSupport;
 
-class UpdateSupportDTO
+class CreateSupportDTO
 {
     public function __construct(
-        public string $id,
         public string $subject,
         public SupportStatus $status,
         public string $body,
     ) {
     }
 
-    public static function makeFromRequest(StoreUpdateSupport $request, string $id = null): self
+    public static function makeFromRequest(StoreUpdateSupport $request): self
     {
         return new self(
-            $id ?? $request->id,
             $request->subject,
             SupportStatus::A,
             $request->body,
--- second-course/app/DTO/Supports/UpdateSupportDTO.php
+++ second-course/app/DTO/Supports/UpdateSupportDTO.php
@@ -1,22 +1,24 @@
 <?php
 
-namespace App\DTO;
+namespace App\DTO\Supports;
 
 use App\Enums\SupportStatus;
 use App\Http\Requests\StoreUpdateSupport;
 
-class CreateSupportDTO
+class UpdateSupportDTO
 {
     public function __construct(
+        public string $id,
         public string $subject,
         public SupportStatus $status,
         public string $body,
     ) {
     }
 
-    public static function makeFromRequest(StoreUpdateSupport $request): self
+    public static function makeFromRequest(StoreUpdateSupport $request, string $id = null): self
     {
         return new self(
+            $id ?? $request->id,
             $request->subject,
             SupportStatus::A,
             $request->body,
