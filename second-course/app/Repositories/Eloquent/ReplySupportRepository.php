8a5980c518b3350dbd5deb88a7b9289641cc2c4f
Guilherme-silva0 <guilhermesilvadev0@gmail.com>
2023-12-08 13:16:19 -0300

    refactor: :recycle: get replies with relationships

--- second-course/app/Repositories/Eloquent/ReplySupportRepository.php
+++ second-course/app/Repositories/Eloquent/ReplySupportRepository.php
@@ -17,7 +17,11 @@ class ReplySupportRepository implements ReplyRepositoryInterface
 
     public function getAllBySupportId(string $supportId): array
     {
-        $replies = $this->model->where('support_id', $supportId)->get();
+        $replies = $this->model
+            ->with(['user', 'support'])
+            ->where('support_id', $supportId)
+            ->get();
+            
         return $replies->toArray();
     }
 
