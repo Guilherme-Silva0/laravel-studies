0cca19cfcede79f1dac7567bcd7ddc6e99c875c3
Guilherme-silva0 <guilhermesilvadev0@gmail.com>
2023-12-11 13:45:44 -0300

    feat: :sparkles: create helper get initials of name

--- second-course/app/Helpers/functions.php
+++ second-course/app/Helpers/functions.php
@@ -8,3 +8,20 @@ if(!function_exists('getStatusSupport')) {
         return SupportStatus::fromValue($value);
     }
 }
+
+if(!function_exists('getInitials')) {
+    function getInitials(string $name): string
+    {
+        $words = explode(' ', $name);
+        $initials = '';
+        
+        foreach ($words as $word) {
+            $initials .= strtoupper(substr($word, 0, 1));
+            if (strlen($initials) >= 2) {
+                break;
+            }
+        }
+
+        return $initials;
+    }
+}
