5c3465495179420ebb261bf10911b83f9eb94009
Guilherme-silva0 <guilhermesilvadev0@gmail.com>
2023-12-11 13:50:18 -0300

    refactor: :recycle: get all supports with replies and users

--- second-course/app/Repositories/Eloquent/SupportRepository.php
+++ second-course/app/Repositories/Eloquent/SupportRepository.php
@@ -20,12 +20,17 @@ class SupportRepository implements SupportRepositoryInterface
 
     public function paginate(int $page = 1, int $totalPerPage = 15, string $filter = null): PaginationInterface
     {
-        $result = $this->model->where(function ($query) use ($filter) {
-            if($filter) {
-                $query->where('subject');
-                $query->orWhere('body', 'like', "%{$filter}%");
-            }
-        })
+        $result = $this->model
+            ->with(['replies' => function ($query) {
+                $query->limit(4);
+                $query->latest();
+            }])
+            ->where(function ($query) use ($filter) {
+                if($filter) {
+                    $query->where('subject');
+                    $query->orWhere('body', 'like', "%{$filter}%");
+                }
+            })
         ->paginate($totalPerPage, ['*'], 'page', $page);
         return new PaginationPresenter($result);
     }
