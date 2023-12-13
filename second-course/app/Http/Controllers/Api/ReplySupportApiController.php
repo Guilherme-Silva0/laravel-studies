7b3da5d6892ec3e0118fb1dcfebcbcf4cae8a83d
Guilherme-silva0 <guilhermesilvadev0@gmail.com>
2023-12-13 13:55:40 -0300

    fix: :bug: response created with status 201

--- second-course/app/Http/Controllers/Api/ReplySupportApiController.php
+++ second-course/app/Http/Controllers/Api/ReplySupportApiController.php
@@ -38,6 +38,8 @@ class ReplySupportApiController extends Controller
             CreateReplyDTO::makeFromRequest($request)
         );
 
-        return new ReplySupportResource($reply);
+        return (new ReplySupportResource($reply))
+            ->response()
+            ->setStatusCode(Response::HTTP_CREATED);
     }
 }
