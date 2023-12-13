ed121ce71df111f22dbc7441e8d63ef283994e16
Guilherme-silva0 <guilhermesilvadev0@gmail.com>
2023-12-13 13:26:00 -0300

    feat: :sparkles: create route list all replies of support

--- second-course/routes/api.php
+++ second-course/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\Auth\AuthController;
+use App\Http\Controllers\Api\ReplySupportApiController;
 use App\Http\Controllers\Api\SupportController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -11,4 +12,8 @@ Route::middleware(["auth:sanctum"])->group(function () {
     Route::apiResource('/supports', SupportController::class);
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/me', [AuthController::class, 'me']);
+
+    Route::get('/replies/{supportId}', [ReplySupportApiController::class, 'index']);
+    Route::post('/supports/{id}/replies', [ReplySupportApiController::class, 'store'])->name('replies.store');
+    Route::delete('/supports/{id}/replies/{reply}', [ReplySupportApiController::class, 'destroy'])->name('replies.destroy');
 });
