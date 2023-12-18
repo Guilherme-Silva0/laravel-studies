e01721e7b2b1f294f1451ec4fb993536fde618cf
Guilherme-silva0 <guilhermesilvadev0@gmail.com>
2023-12-18 16:17:36 -0300

    feat: :sparkles: create books.update route

--- api-tdd/routes/api.php
+++ api-tdd/routes/api.php
@@ -11,3 +11,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::get('/books', [BookController::class, 'index'])->name('books.index');
 Route::get('/books/{bookId}', [BookController::class, 'show'])->name('books.show');
 Route::post('/books', [BookController::class, 'store'])->name('books.store');
+Route::match(['put', 'patch'], '/books/{bookId}', [BookController::class, 'update'])->name('books.update');
