54dd129fe31b98c0c92fb54573208fceb41bb057
Guilherme-silva0 <guilhermesilvadev0@gmail.com>
2023-12-18 19:53:31 -0300

    refactor: :recycle: use resources api routes

--- api-tdd/routes/api.php
+++ api-tdd/routes/api.php
@@ -8,8 +8,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/books', [BookController::class, 'index'])->name('books.index');
-Route::get('/books/{bookId}', [BookController::class, 'show'])->name('books.show');
-Route::post('/books', [BookController::class, 'store'])->name('books.store');
-Route::match(['put', 'patch'], '/books/{bookId}', [BookController::class, 'update'])->name('books.update');
-Route::delete('/books/{bookId}', [BookController::class, 'destroy'])->name('books.destroy');
+Route::apiResource('books', BookController::class);
