300b378c744fdc682636bc8545019b413125c16c
Guilherme-silva0 <guilhermesilvadev0@gmail.com>
2023-12-18 14:59:48 -0300

    feat: :sparkles: create show route

--- api-tdd/routes/api.php
+++ api-tdd/routes/api.php
@@ -9,3 +9,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('/books', [BookController::class, 'index'])->name('books.index');
+Route::get('/books/{bookId}', [BookController::class, 'show'])->name('books.show');
