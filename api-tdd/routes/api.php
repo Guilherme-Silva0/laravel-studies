4d69334d0f096bf76184c4c03e8157a09bc95fa3
Guilherme-silva0 <guilhermesilvadev0@gmail.com>
2023-12-18 18:33:34 -0300

    feat: :sparkles: add route books.destroy

--- api-tdd/routes/api.php
+++ api-tdd/routes/api.php
@@ -12,3 +12,4 @@ Route::get('/books', [BookController::class, 'index'])->name('books.index');
 Route::get('/books/{bookId}', [BookController::class, 'show'])->name('books.show');
 Route::post('/books', [BookController::class, 'store'])->name('books.store');
 Route::match(['put', 'patch'], '/books/{bookId}', [BookController::class, 'update'])->name('books.update');
+Route::delete('/books/{bookId}', [BookController::class, 'destroy'])->name('books.destroy');
