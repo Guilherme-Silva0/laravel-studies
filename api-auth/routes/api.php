05bd5e63c9d2cc28e393bf643e4f35b8562d6ffe
Guilherme-silva0 <guilhermesilvadev0@gmail.com>
2023-12-19 17:37:49 -0300

    feat: :sparkles: create route  user.register

--- api-auth/routes/api.php
+++ api-auth/routes/api.php
@@ -4,3 +4,4 @@ use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [UserController::class, 'login'])->name('user.login');
+Route::post('/register', [UserController::class, 'register'])->name('user.register');
