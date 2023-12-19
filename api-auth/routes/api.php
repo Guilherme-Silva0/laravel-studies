8ba30aa5fc444351037075ad2671f11b9fda538f
Guilherme-silva0 <guilhermesilvadev0@gmail.com>
2023-12-19 17:42:10 -0300

    feat: :sparkles: create route user.profile

--- api-auth/routes/api.php
+++ api-auth/routes/api.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [UserController::class, 'login'])->name('user.login');
 Route::post('/register', [UserController::class, 'register'])->name('user.register');
+
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    Route::get('/profile', [UserController::class, 'profile'])->name('user.profile');
+});
