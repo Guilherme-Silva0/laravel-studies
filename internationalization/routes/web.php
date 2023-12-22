f58ddd2a8eff3a6595072c37d492e56b722085cb
Guilherme-silva0 <guilhermesilvadev0@gmail.com>
2023-12-22 19:34:26 -0300

    feat: :sparkles: add set_land route

--- internationalization/routes/web.php
+++ internationalization/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +16,19 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    App::setLocale(session()->get('locale') ?? 'en');
     return view('welcome');
 });
+
+Route::get('/set_lang', function (Request $request) {
+
+    $lang = $request->get('lang');
+
+    if (! in_array($lang, ['en', 'pt_BR'])) {
+        abort(400);
+    }
+
+    session()->put('locale', $lang);
+
+    return redirect()->back();
+})->name('set_lang');
